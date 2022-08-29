9a916772128233b06510ef73f543a4be6ded0264
vincenzoDiMarco84 <vincenzodimarco1984@hotmail.it>
2022-08-29 09:15:34 +0200

    final commit

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 30;
+const count = 5;
 const apiKey = "oxdcNf7d7ZqVqLuEVtqmfDl8IPkXpNn7r1uqm-Vq8TA";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
@@ -18,6 +18,7 @@ function imageLoaded() {
   if (imagesLoaded === totalImages) {
     ready = true;
     loader.hidden = true;
+    count = 30;
   }
 }
 
